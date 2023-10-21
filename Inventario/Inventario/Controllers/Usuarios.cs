67450cc43485aa8319102b070c401a93c14e044d
Dern965 <dern965@hotmail.com>
2023-10-21 14:56:49 -0600

    si

---
+++ Inventario/Inventario/Controllers/Usuarios.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventario.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Usuarios : ControllerBase
+    {
+    }
+}
