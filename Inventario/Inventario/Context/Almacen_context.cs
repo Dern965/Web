931e8021a6f1f3a22ac85a82aa99b73d0f76074a
Dern965 <dern965@hotmail.com>
2023-11-12 20:35:39 -0600

    si

---
+++ Inventario/Inventario/Context/Almacen_context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Inventario.Clases;
+
+namespace Inventario.Context
+{
+    public class Almacen_context : DbContext
+    {
+        public DbSet<Producto> productos {  get; set; }
+        public DbSet<Usuario> usuarios { get; set; }
+
+        public Almacen_context(DbContextOptions<Almacen_context> options) : base(options) { }
+    }
+}
